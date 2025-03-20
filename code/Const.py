3b616b754e846d8f81033e7d0b84710f7c7dc2d4
AlanPatrik-iLAB <alan.santos@ilabqa.com>
2025-03-20 19:32:33 -0300

    adding configuration constant variables

---
+++ code/Const.py
@@ -0,0 +1,11 @@
+# Configurações do jogo
+SCREEN_WIDTH = 600
+SCREEN_HEIGHT = 600
+CELL_SIZE = 20
+FPS = 15
+
+# Cores
+BLACK = (0, 0, 0)
+WHITE = (255, 255, 255)
+GREEN = (0, 255, 0)
+RED = (255, 0, 0)
